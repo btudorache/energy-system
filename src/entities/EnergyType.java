a97d807af4f4bfd97806c3cfcbc23df7d420cc3e
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-01-13 12:14:55 +0200

    added skel entity

---
+++ src/entities/EnergyType.java
@@ -0,0 +1,45 @@
+package entities;
+
+/**
+ * Types of energy produced by EnergyProducers
+ */
+public enum EnergyType {
+    WIND("WIND", true),
+    SOLAR("SOLAR", true),
+    HYDRO("HYDRO", true),
+    COAL("COAL", false),
+    NUCLEAR("NUCLEAR", false);
+
+    private final String label;
+
+    private final boolean renewable;
+
+    EnergyType(final String label, final boolean renewable) {
+        this.label = label;
+        this.renewable = renewable;
+    }
+
+    /**
+     *
+     * @param type
+     * @return
+     */
+    public static EnergyType getEnergyType(final String type) {
+        return switch (type) {
+            case "WIND" -> EnergyType.WIND;
+            case "SOLAR" -> EnergyType.SOLAR;
+            case "HYDRO" -> EnergyType.HYDRO;
+            case "COAL" -> EnergyType.COAL;
+            case "NUCLEAR" -> EnergyType.NUCLEAR;
+            default -> null;
+        };
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public boolean isRenewable() {
+        return renewable;
+    }
+}
