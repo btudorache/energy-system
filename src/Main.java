98e42f0eac37ce6593632079048168feb8492c08
Bogdan Tudorache <bogdanmihait10@gmail.com>
2021-01-13 12:26:51 +0200

    added main entrypoint of the project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import fileio.JsonFileIO;
+import fileio.output.SimulationResult;
+
+public final class Main {
+    private Main() { }
+
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    public static void main(final String[] args) throws Exception {
+        JsonFileIO fileIO = new JsonFileIO(args[0], args[1]);
+        Simulation simulation = Simulation.getInstance(fileIO.getData());
+        SimulationResult results = simulation.runSimulation();
+        fileIO.writeData(results);
+
+        Simulation.clearInstance();
+    }
+}
